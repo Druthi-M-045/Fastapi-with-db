7664f23edb77b8853f7b167a84905df0d0f45931
Druthi-M-045 <druthimsgowda@gmail.com>
2026-02-07 09:47:54 +0530

    Fastapi

--- routers/user_routes.py
+++ routers/user_routes.py
@@ -3,14 +3,17 @@ from sqlalchemy.orm import Session
 from fastapi import Depends
 from db import get_db
 from repositories.user_repo import UserRepo
-from schemas.User_schemas import UserSchema
+from schemas.user_schemas import UserSchema
+
+from models import User
 
 router = APIRouter()
 
-@router.post("/users")
-def signup(User:User,db: Session=Depends(get_db)):
+@router.post("/signup")
+def signup(user_request: UserSchema, db: Session = Depends(get_db)):
+    new_user = User(email=user_request.email, password=user_request.password)
     user_repo = UserRepo(db)
-    user_repo.add_user()
+    user_repo.add_user(new_user)
     return {"message": "User signup successfully"}
 
 @router.post("/login")
