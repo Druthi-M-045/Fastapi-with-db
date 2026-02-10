6da3270eefa0f7d72787df6b00d308a0857d9c9a
Druthi-M-045 <druthimsgowda@gmail.com>
2026-02-10 13:08:14 +0530

    FastAPI

---
+++ utils/email_sender.py
@@ -0,0 +1,29 @@
+import smtplib
+from email.message import EmailMessage
+from dotenv import load_dotenv
+import os
+load_dotenv()
+app_password=os.getenv("APP_PASSWORD")
+sender_email=os.getenv("SENDER_EMAIL")
+
+# Email details
+def send_email(receiver_email: str, subject: str, content: str) -> str:
+    """
+    Send an email to the specified receiver.
+    """
+
+# Create email
+    msg = EmailMessage()
+    msg["From"] = sender_email
+    msg["To"] = receiver_email
+    msg["Subject"] = subject
+    msg.set_content(content)
+
+    # Send email
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()  # secure connection
+        server.login(sender_email, app_password)
+        server.send_message(msg)
+
+    print("Email sent successfully!")
+
