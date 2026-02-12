b926c9c5258992ecdc170796d2b443ee844a9c9b
Druthi-M-045 <druthimsgowda@gmail.com>
2026-02-12 15:02:50 +0530

    tyu

--- dependencies.py
+++ dependencies.py
@@ -1,11 +1,13 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
+from typing import Optional
 from db import get_db
 from models import User
 from utils.jwt_handler import verify_token
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+oauth2_scheme_optional = OAuth2PasswordBearer(tokenUrl="login", auto_error=False)
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     payload = verify_token(token)
@@ -22,4 +24,19 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
     user = db.query(User).filter(User.email == email).first()
     if user is None:
         raise HTTPException(status_code=401, detail="User not found")
+    return user
+
+def get_current_user_optional(token: Optional[str] = Depends(oauth2_scheme_optional), db: Session = Depends(get_db)) -> Optional[User]:
+    if not token:
+        return None
+    
+    payload = verify_token(token)
+    if not payload:
+        return None
+        
+    email: str = payload.get("email")
+    if email is None:
+        return None
+    
+    user = db.query(User).filter(User.email == email).first()
     return user
